f11c2a80b565f8f934829c7a4cb08ba151efd811
francespos <f.esposito.email@gmail.com>
2025-11-14 22:24:34 +0100

    .

--- test/test_layer.cpp
+++ test/test_layer.cpp
@@ -5,20 +5,82 @@
 
 const float epsilon = 1e-6f;
 
+deepc_layer layer;
+
+float weights[] = 
+{
+    1.0f, -2.0f, 2.0f, 
+    -3.0f, 3.0f, -4.0f
+};
+
+float biases[] = {2.0f, -3.0f};
+
+const size_t input_size = 3;
+const size_t output_size = 2;
+
+const float input[] = {1.0f, -2.0f, 3.0f};
+float z[2];
+float output[2];
+
+void matvec_mul(float* rslt, const float* m, const float* v, size_t nr, 
+    size_t n)
+{
+    for (size_t i = 0; i < nr; ++i)
+    {
+        rslt[i] = 0.0f; 
+
+        for (size_t j = 0; j < n; ++j)
+        {
+            rslt[i] += m[i * n + j] * v[j];
+        }
+    }
+}
+
+void vec_sum(float* rslt, const float* v1, const float* v2, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        rslt[i] = v1[i] + v2[i]; 
+    }
+}
+
+void vec_copy(float* dest, const float* src, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        dest[i] = src[i];
+    }
+}
+
+void vec_apply_func(float* rslt, float (*func)(float), const float* v, 
+    size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        rslt[i] = func(v[i]);
+    }
+}
+
+void test_dense_layer_forward()
+{
+    layer.forward(output, z, &layer, input); 
+
+    float true_z[2]; 
+    matvec_mul(true_z, weights, input, output_size, input_size);
+    vec_sum(true_z, true_z, biases, 2);
+
+    assert(deepc_float_arrays_equal(z, true_z, 2, epsilon));
+    
+    float true_output[2];
+    vec_apply_func(true_output, deepc_sigmoid, true_z, 2);  
+
+    assert(deepc_float_arrays_equal(output, true_output, 2, epsilon));
+}
+
 int main()
 {
-    float weights[] = 
-    {
-        1.0f, -2.0f, 2.0f, 
-        -3.0f, 3.0f, -4.0f
-    };
-
-    float biases[] = {2.0f, -3.0f};
-
-    size_t input_size = 3;
-    size_t output_size = 2;
-
-    deepc_layer layer;
     deepc_dense_layer_initialize(&layer, weights, biases, input_size, 
-        output_size, deepc_relu, deepc_relu_derivative);
+        output_size, deepc_sigmoid, deepc_sigmoid_derivative);
+
+    test_dense_layer_forward();
 }
